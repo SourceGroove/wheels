8909dc319657c0dcfc8940b2924427623792016a
Seth Jordan <sjordan@madenasolutions.com>
2020-03-23 23:25:51 -0400

    init

---
+++ friends.py
@@ -0,0 +1,11 @@
+# Hadley's first python program
+if __name__ == "__main__":
+  
+    names = ['tyler','nat','chloe','seth' ,'hannah','alex','charlie']
+
+    print('---------------------------------------------')
+    print(' i have + str(len(names)) friends')
+    # I have 7 friends
+    
+    for name in names:
+        print('hello my name is ' + name)
